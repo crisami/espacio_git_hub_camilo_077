4c394dc74d23b19da77641ac2cff3a25e9e0f7c0
narvaez <narvaez@gmai.com>
2015-05-17 17:25:21 +0200

    primera version del proyecto tienda

---
+++ tienda_camilo007/src/tienda/Principal.java
@@ -0,0 +1,27 @@
+package tienda;
+
+
+public class Principal {
+
+	Productos producto_Uno = new Productos();
+	Productos producto_Dos = new Productos();
+	Productos producto_Tres = new Productos();
+	
+	Clientes cliente_Uno = new Clientes();
+	Clientes cliente_Dos = new Clientes();
+	Clientes cliente_Tres = new Clientes();
+	
+	Vendedores vendedor_Uno = new Vendedores();
+	Vendedores vendedor_Dos = new Vendedores();
+	Vendedores vendedor_Tres = new Vendedores();
+	
+	Tiendas tienda_uno = new Tiendas();
+	Tiendas tienda_Dos = new Tiendas();
+	Tiendas tienda_Tres = new Tiendas();
+	
+	public static void main(String[]args){
+		
+		
+	}
+	
+}
---
+++ tienda_camilo007/src/tienda/Venta.java
@@ -0,0 +1,23 @@
+package tienda;
+
+public class Venta {
+
+	private int fecha;
+	private int hora;
+
+	int getHora() {
+		return hora;
+	}
+
+	void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	int getFecha() {
+		return fecha;
+	}
+
+	void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+}
